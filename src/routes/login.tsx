53ef429ca57c3593af9b6f0cdd88a14e276007cd
Christoffer Artmann <Artgaard@gmail.com>
2019-09-29 18:12:48 +0200

    wip: Add a route for user login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
-// @ts-ignore
+//@ts-ignore
 import SpotifyLogin from 'react-spotify-login';
 import styled from 'styled-components';
+
 
 interface ILoginProps extends RouteComponentProps<any> {}
 
@@ -22,25 +23,34 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: 2rem;
+  padding: 4rem 3rem;
+
+  .loginButton {
+    background: #1db954;
+    border: none;
+    border-radius: 6px;
+    color: #fff;
+    padding: 0.5rem 1rem;
+  }
 `;
 const Image = styled.img`
   height: auto;
-  width: 16rem;
+  margin-bottom: 3rem;
+  width: 8rem;
 `;
 
 export default function Login({ history }: ILoginProps) {
   const onSuccess = ({ access_token: accessToken }: any) => {
     localStorage.setItem('token', accessToken);
-  };
-  const onFailure = (response: any) => console.error({
 
-  });
+    history.push('/');
+  };
+  const onFailure = (response: any) => console.error(response);
 
   return (
     <Background>
       <Container>
-        <Image src="https://developer.spotify.com/assets/branding-guidelines/icon1@2x.png" />
+        <Image src="/images/Spotify_Icon_RGB_Green.png" />
         <SpotifyLogin
           clientId="2dd19ce71d014c41843e36872d568659"
           redirectUri="http://localhost:3000"
